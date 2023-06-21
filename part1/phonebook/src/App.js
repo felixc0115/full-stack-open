4f2d777c4ab5a6fbc06927a032de49d2490257fd
Felix Chen <fechen1993@gmail.com>
2023-06-20 21:27:29 -0400

    submit exercise 2.6

--- part1/phonebook/src/App.js
+++ part1/phonebook/src/App.js
@@ -6,19 +6,31 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  const nameChangeHandler = (e) => setNewName(e.target.value);
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={nameChangeHandler} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={submitHandler} type="submit">
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
