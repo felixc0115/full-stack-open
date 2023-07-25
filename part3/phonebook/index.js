952508258f697d7f1bae63c6132dfdba9a5910c0
Felix Chen <fechen1993@gmail.com>
2023-07-25 10:30:58 -0400

    submit exercise 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -10,6 +10,7 @@ morgan.token("body", function (req, res) {
 });
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use(
   morgan(function (tokens, req, res) {
@@ -26,8 +27,6 @@ app.use(
   })
 );
 
-app.use(express.static("build"));
-
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((people) => {
     res.json(people);
@@ -40,7 +39,7 @@ app.get("/info", (req, res) => {
   );
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
@@ -50,8 +49,7 @@ app.get("/api/persons/:id", (req, res) => {
       }
     })
     .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: "malformatted id" });
+      next(error);
     });
 });
 
@@ -74,6 +72,35 @@ app.post("/api/persons", (req, res) => {
     console.log(result);
   });
 });
+
+app.put("api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
