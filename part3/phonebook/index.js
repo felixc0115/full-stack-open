e3d1a2f49967c53dc77598e44d9567017ae96cd7
Felix Chen <fechen1993@gmail.com>
2023-07-26 21:34:22 -0400

    submit exercise 3.17

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -79,12 +79,11 @@ app.put("/api/persons/:id", (req, res, next) => {
     name: body.name,
     number: body.number,
   };
-  Person.findByIdAndUpdate(req.params.id, person, { new: true }).then(
-    (updatedPerson) => {
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
       res.json(updatedPerson);
-    }
-  );
-  // .catch((error) => next(error));
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (req, res) => {
