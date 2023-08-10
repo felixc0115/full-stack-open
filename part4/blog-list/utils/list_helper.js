394cedc7e3a0f8b4391c157ffaaa10838b0ccf82
Felix Chen <fechen1993@gmail.com>
2023-08-10 14:10:55 -0400

    submit exercise 4.4 - create unit tests for total likes

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,5 +1,5 @@
-const dummy = (blogs) => {
-  return 1;
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, current) => total + current.likes, 0);
 };
 
-module.exports = { dummy };
+module.exports = { totalLikes };
