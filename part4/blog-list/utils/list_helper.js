1923370ab8465bdea5392a236d8579e82b560f2a
Felix Chen <fechen1993@gmail.com>
2023-08-11 11:37:36 -0400

    submit exercise 4.5 - favoriteBlog unit test

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,4 +2,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, current) => total + current.likes, 0);
 };
 
-module.exports = { totalLikes };
+const findFavorite = (blogs) => {
+  let mostLiked = blogs.reduce(
+    (most, current) => (current.likes > most.likes ? current : most),
+    { title: "", author: "", likes: 0 }
+  );
+  ({ title, author, likes } = mostLiked);
+  return { title, author, likes };
+};
+
+module.exports = { totalLikes, findFavorite };
